bc5f7962ded131299be0572702ab4f44bb7816a6
mothibimalebana <mothibi.mm12@gmail.com>
2024-11-18 15:47:44 +0200

    created a dom variables that connect to navRight, navLeft
    elements will be appended to the created variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-/** DOM ELEMENTS: */
-const navRight = document.querySelector('.navRight')
+import './css/home.css'
 
+const screenController = (function (){
+    /** DOM Elements: */
+    const navRight = document.querySelector('.navRight');
+    const navLeft = document.querySelector('.navLeft');
+
+    const contentLeft = document.querySelector('.contentLeft')
+
+    return{navLeft, navRight, contentLeft}
+})();
+
+console.log(screenController.contentLeft)
 
