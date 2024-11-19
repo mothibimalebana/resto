0628a2d036f6848aa7e58887eaea93576ae22ce6
mothibimalebana <mothibi.mm12@gmail.com>
2024-11-19 19:03:41 +0200

    created menuContent(dom)

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,5 +2,9 @@ import '../css/home.css';
 import granola from "../img/granola.png";
 
 const menuPage = (function (){
+    /**DOM Elements */
+    const menuContent = document.createElement("div");
+    menuContent.classList.add("menuContent");
 
+    
 })();
